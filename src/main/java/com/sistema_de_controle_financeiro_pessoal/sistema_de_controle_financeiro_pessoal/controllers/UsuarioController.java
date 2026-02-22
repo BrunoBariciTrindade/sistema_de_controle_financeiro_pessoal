855e290db8730998d01a253ec3481d3f1667bf23
bruno <brbaricitrin@yahoo.com>
2026-02-22 09:22:53 -0300

    add usuarioController

---
+++ src/main/java/com/sistema_de_controle_financeiro_pessoal/sistema_de_controle_financeiro_pessoal/controllers/UsuarioController.java
@@ -0,0 +1,10 @@
+package com.sistema_de_controle_financeiro_pessoal.sistema_de_controle_financeiro_pessoal.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/CadatroUsuario")
+public class UsuarioController {
+
+}
