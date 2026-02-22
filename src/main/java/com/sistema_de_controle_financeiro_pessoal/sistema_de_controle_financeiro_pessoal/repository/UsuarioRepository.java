053d4a75471b1686b36f096ade8d9f7f19c4c98f
bruno <brbaricitrin@yahoo.com>
2026-02-22 09:18:33 -0300

    integrado jpa em repositorio usuario

--- src/main/java/com/sistema_de_controle_financeiro_pessoal/sistema_de_controle_financeiro_pessoal/repository/UsuarioRepository.java
+++ src/main/java/com/sistema_de_controle_financeiro_pessoal/sistema_de_controle_financeiro_pessoal/repository/UsuarioRepository.java
@@ -1,5 +1,11 @@
 package com.sistema_de_controle_financeiro_pessoal.sistema_de_controle_financeiro_pessoal.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistema_de_controle_financeiro_pessoal.sistema_de_controle_financeiro_pessoal.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 
 }
